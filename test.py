a49699c3e79d918d8797bd73c7808ebb5753a089
Nick <nbp2992@gmail.com>
2026-06-29 02:30:09 -0400

    chore: remove unwanted file

--- test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-with open("/home/nick/system.log", "r") as testfilevar:
-    for line in testfilevar:
-        if "INFO" in line:
-            continue
-        else:
-            print("[ALERT FOUND] " + line )
